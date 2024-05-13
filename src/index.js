0c1aa732e9dccb62ab3e62cd9a5bb584fbb1ff1c
mjnhloc <minhloc6112003@gmail.com>
2024-05-13 22:55:36 +0700

    Routes and Controller

--- src/index.js
+++ src/index.js
@@ -2,30 +2,37 @@ const path = require('path')
 const express = require('express')
 const { engine } = require('express-handlebars')
 const morgan = require('morgan')
+const route = require('./routes')
 
 const app = express()
 const port = 3000
 
+// body parse
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+app.use(express.json())
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // Template engine
-app.engine('hbs', engine({
-  extname: ".hbs"
-}))
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+)
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+// Routes init
+route(app)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
